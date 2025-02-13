7190afe4fa9359db109c97a587027da7448405f8
Daniel Gonzalez <daniel.gonzalezubaque@gmail.com>
2025-02-13 15:00:25 -0500

    script for upload music

---
+++ upload music/upload_music.py
@@ -0,0 +1,31 @@
+import os
+from supabase import create_client
+from dotenv import load_dotenv
+
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def upload_mp3_to_supabase(file_path, title, artist, image_url):
+    try:
+        file_name = os.path.basename(file_path)
+        supabase.storage.from_("musics").upload(file_name, file_path)
+        file_url = supabase.storage.from_("musics").get_public_url(file_name)
+        data = {
+            "title": title,
+            "artist": artist,
+            "file_url": file_url,
+            "image_url": image_url,
+        }
+        supabase.table("songs").insert(data).execute()
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    file_path = ".mp3"
+    title = ""
+    artist = ""
+    image_url = ""
+    
+    upload_mp3_to_supabase(file_path, title, artist, image_url)
